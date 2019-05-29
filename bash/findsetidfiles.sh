ca54d2105e2cfcdc2cf7e92572e9e18ea06e54bd
vinayreddy113 <vinayreddy113@gmail.com>
2019-05-29 10:37:45 -0700

    Updated

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-#
 # this script generates a report of the files on the system that have the setuid permission bit turned on
 # it is for the lab1 exercise
-
 # Task 1 for the exercise is to modify it to also find and display the setgid files in a second listing
 # The second listing should display after the setuid file list, and be formatted similar to the setuid file list
 
@@ -11,9 +9,7 @@
 
 echo "Setuid files:"
 echo "============="
-find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3   # find use to find file location -type f mean only files sort -k 3
-                                                                        #                                 mean sort the output by key 3
-
+find / -type f -executable -perm -4000 -ls 2>/dev/null | sort -k 3
 
 echo ""
 
@@ -32,4 +28,10 @@ sleep 5
 
 find / 2>/dev/null -type f -exec ls -l --block-size=M {} + | sort -rh -k 5 | head -n 10 | awk '{print $5, $3, $9}'
 
-                                                                 
+
+#ls -l --block-size=M / 2>/dev/null | sort -h -r | head -n 10
+
+#du --block-size=M / 2>/dev/null | sort -h -r | head -n 10
+
+du -ha | sort -h -r | head -n 10
+#find -type f -exec ls -l --block-size=M {} + | sort -rh -k 5 | head -n 10 | awk '{print $5, $3, $9}'
